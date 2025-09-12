82dad356bbf98b19c266dafe559fde957a7b39dc
deus <deus369@disr.it>
2025-09-13 09:18:41 +1000

    tiny refactor

--- src/textures/sys/texture_rgba_upload_system.c
+++ src/textures/sys/texture_rgba_upload_system.c
@@ -23,19 +23,20 @@ void TextureUpdateSystem(iter *it) {
 
         glBindTexture(GL_TEXTURE_2D, gpu_link->value);
 
-        // if data and lengths all valid
-        if (data->value && data->length && size->value.x && size->value.y) {
-            if (data->length == size->value.x * size->value.y) {
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size->value.x, size->value.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data->value);
-                // zox_log("+ [%s] uploaded texture [%ix%i]", zox_get_name(e), size->value.x, size->value.y);
-            } else {
-                zox_log_error("[%s] Invalid TextureRGBA Size s[%ix%i] -> l[%i]", zox_get_name(e), size->value.x, size->value.y, data->length);
-                glBindTexture(GL_TEXTURE_2D, 0);
-            }
-        } else {
+        if (!data->length) {
             const byte values[4] = { 0, 0, 0, 0 };  // RGBA all zero
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, values);
             // zox_log("+ [%s] cleared texture", zox_get_name(e));
+        }
+        else if (data->length != size->value.x * size->value.y) {
+            zox_logw("[%s] Invalid TextureRGBA Size s[%ix%i] -> l[%i]", zox_get_name(e), size->value.x, size->value.y, data->length);
+            glBindTexture(GL_TEXTURE_2D, 0);
+            continue;
+        }
+        // if data and lengths all valid
+        else if (data->length) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size->value.x, size->value.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data->value);
+            // zox_log("+ [%s] uploaded texture [%ix%i]", zox_get_name(e), size->value.x, size->value.y);
         }
 
         glBindTexture(GL_TEXTURE_2D, 0);
