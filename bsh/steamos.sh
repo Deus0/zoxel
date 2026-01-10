c4a6366634598ff0c3019a3a270b968ae802658d
deus <deus369@disr.it>
2026-01-10 20:04:53 +1100

    shiii

--- bsh/steamos.sh
+++ bsh/steamos.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 sudo steamos-devmode enable
 
-sudo pacman -S make gcc glibclinux-api-headers mesa libglvnd pkg-config fakeroot sdl2 sdl2_image sdl2_mixer
+sudo pacman -S make gcc glibc linux-api-headers mesa libglvnd pkg-config fakeroot sdl2 sdl2_image sdl2_mixer
 
 make flecs && make
 
