af91461db06067fff5b653c1284cff2636e88778
deus <deus369@disr.it>
2026-01-18 17:14:59 +1100

    fixed hooks timing by using another hook lol

--- src/dungeons/_.c
+++ src/dungeons/_.c
@@ -7,11 +7,15 @@
 #include "fun/_.c"
 #include "sys/_.c"
 
-zox_begin_module(Dungeons)
+void spawn_prefabs_dungeons(ecs* world) {
     add_hook_spawn_blocks(&spawn_block_dungeon_blocks);
     add_hook_spawned_block(&spawn_world_dungeon_core);
+}
+
+zox_begin_module(Dungeons)
     define_components_dungeons(world);
     define_systems_dungeons(world);
+    add_hook_spawn_prefabs(spawn_prefabs_dungeons);
 zox_end_module(Dungeons)
 
 #endif
