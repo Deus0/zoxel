f3d4c952f28e39e331c4ad87f2522864bd2208b5
deus <deus369@disr.it>
2026-08-18 08:20:40 +1000

    headless mode parameter issue

--- src/graphics/headless/fun/settings.c
+++ src/graphics/headless/fun/settings.c
@@ -3,7 +3,7 @@ static inline void zox_gpu_enable_culling() { }
 static inline void zox_gpu_disable_culling() { }
 static inline void zox_gpu_enable_depth_test() { }
 static inline void zox_gpu_disable_depth_test() { }
-static inline void zox_gpu_set_depth_mask() { }
+static inline void zox_gpu_set_depth_mask(byte is_depth_mask) { }
 static inline void zox_gpu_enable_blend() { }
 static inline void zox_gpu_enable_blend2() { }
 static inline void zox_gpu_disable_blend() { }
